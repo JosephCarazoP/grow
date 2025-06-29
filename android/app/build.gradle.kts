70741e6faf64ffa70b9de430c5267eed73058212
JosephCarazoP <josephcarazo56@gmail.com>
2025-06-28 18:05:48 -0600

    Se trabajan las notificaciones de la app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.firebase:firebase-messaging:23.1.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 android {
@@ -27,6 +28,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
